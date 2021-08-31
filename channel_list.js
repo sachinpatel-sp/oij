0d14c66dd369f9251f20a420aecc837b5cabba21
Sachin Patel <47366509+sachinpatel-sp@users.noreply.github.com>
2021-09-01 02:18:05 +0530

    Add files via upload

---
+++ channel_list.js
@@ -0,0 +1,37 @@
+const api_url =
+  "https://sgdccdnems03.cdnsrv.jio.com/jiotv.data.cdn.jio.com/apis/v1.3/getMobileChannelList/get/?langId=6&os=android&devicetype=phone&usergroup=tvYR7NSNn7rymo3F&version=6.0.8&langId=6";
+
+async function view(id) {
+     const m3u8 = "http://152.67.9.211//jioo.php?c=" + id + "&q=800&e=junk.m3u8";
+     console.log(m3u8);
+  window.open(m3u8, "_parent");
+}
+
+async function getapi(url) {
+  // Storing response
+  const response = await fetch(url);
+  const ch_list = document.getElementById("ch_list");
+  // Storing data in form of JSON
+  let data = await response.json();
+
+  for (let i = 0; i < 893; i++) {
+    var div = document.createElement("div");
+    var img = document.createElement("img");
+    var img_val =
+      "http://jiotv.catchup.cdn.jio.com/dare_images/images/" +
+      data.result[i].logoUrl;
+    img.setAttribute("src", img_val);
+    div.id = data.result[i].logoUrl.substring(
+      0,
+      data.result[i].logoUrl.indexOf(".")
+    );
+    div.addEventListener("click", function () {
+      view(this.id);
+    });
+    div.className = "channel";
+    div.appendChild(img);
+    ch_list.appendChild(div);
+  }
+}
+// Calling that async function
+getapi(api_url);
